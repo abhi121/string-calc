4dc761fb6fe7865fabb069a1bdbce4305883cd9a
Abhinav Sharma <troy@192.168.1.25>
2025-04-22 16:01:25 +0530

    Refactor: newlines b/w numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,6 @@ class StringCalculator
         return 0 if numbers.empty?
         return numbers.to_i if numbers == numbers.to_i.to_s
 
-        numbers.split(',').map(&:to_i).sum
+        numbers.split(',').map{ |x| x.split("\n")}.flatten.map(&:to_i).sum
     end
 end
