b8c4ed62c019252d005eadb65e03a06a54d2d6b8
Abhinav Sharma <troy@192.168.1.25>
2025-04-22 16:10:27 +0530

    Code: Custom Delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,13 @@ class StringCalculator
         return 0 if numbers.empty?
         return numbers.to_i if numbers == numbers.to_i.to_s
 
-        numbers.split(',').map{ |x| x.split("\n")}.flatten.map(&:to_i).sum
+        if numbers.start_with?("//")
+            end_index = numbers.index("\n")
+            new_delimiter = numbers[2...end_index]
+            new_string = numbers[(end_index + 1)..]
+            new_string.split(new_delimiter).map(&:to_i).sum
+        else
+            numbers.split(',').map{ |x| x.split("\n")}.flatten.map(&:to_i).sum
+        end
     end
 end
